0ef790f61208c07238c8e74920e641c6f2e4152a
Wojciech Jabłoński <wj359634@students.mimuw.edu.pl>
2021-04-16 16:34:45 +0200

    even better docs

--- README.md
+++ README.md
@@ -11,14 +11,30 @@ repeating if the safeguard is bypassed!
 Or read and run `main.cc`
 ```
 root.u64()                3b2c8aefd44be966
+
+// every node is both an rng value and a tree node
 root[0].u64()             d28b1dc8bd364bab
 root[1].u64()             d7a2756ea4d7570c
 root[2].u64()             bdec7adcd3c7ea3c
-root[2]["foo"] .u64()     a2fa7d19fdd4accf
+
+// persistent values!
+root[0].u64()             d28b1dc8bd364bab
+
+// string accessors
+root[2]["foo"].u64()      a2fa7d19fdd4accf
 root[0][3].u64()          a4c48a81aef5fb92
 root[1][3].u64()          4a4d43fc8f51179b
+
+// mixing strings and integers
 root[2]["bar"][3].u64()   265a8341d299e669
+
+// unlimited (and almost performance free) levels
 root[3][1][4][1].u64()    a56fc7ebeeed6c21
+
+// persistent in arbitrarily deep paths
+root[0][3].u64()          a4c48a81aef5fb92
+
+// reshuffling of subtrees
 root[42].u64()            e4eee199af6a0ea0
 root[42].reshuffle()      reshuffle
 root[42].u64()            1bcf06cd0587fb16
--- main.cc
+++ main.cc
@@ -6,31 +6,35 @@ using namespace TR;
 
 int main ()
 {
-    TRNG trng(1337); // seed optional!
+    TRNG trng(0); // seed optional!
     Node root = trng.get_root();
 
-    printf("%-20s%llx\n", "root", root.u64());
+    printf("%-20s%lx\n", "root", root.u64());
 
     // node can be simultanousely used as an rng and for its children
-    printf("%-20s%llx\n", "root[0]", root[0].u64());
-    printf("%-20s%llx\n", "root[1]", root[1].u64());
-    printf("%-20s%llx\n", "root[2]", root[3].u64());
+    printf("%-20s%lx\n", "root[0]", root[0].u64());
+    printf("%-20s%lx\n", "root[1]", root[1].u64());
+    printf("%-20s%lx\n", "root[2]", root[3].u64());
+    // values are persistent!
+    printf("%-20s%lx\n", "root[0]", root[0].u64());
 
     // string and int identifiers cannot be mixed in [] accessors of a given node
     // (it may result in rng values repeating). Some checks are in place to safeguard
     // so for example root["bar"] would fail.
-    printf("%-20s%llx\n", "root[2][\"foo\"]", root[3]["foo"].u64());
+    printf("%-20s%lx\n", "root[2][\"foo\"]", root[3]["foo"].u64());
 
     // multi-level access
-    printf("%-20s%llx\n", "root[0][3]", root[0][3].u64());
-    printf("%-20s%llx\n", "root[1][3]", root[1][3].u64());
-    printf("%-20s%llx\n", "root[2][\"bar\"][3]", root[1]["bar"][3].u64());
-    printf("%-20s%llx\n", "root[3][1][4][1]", root[3][1][4][1].u64());
+    printf("%-20s%lx\n", "root[0][3]", root[0][3].u64());
+    printf("%-20s%lx\n", "root[1][3]", root[1][3].u64());
+    printf("%-20s%lx\n", "root[2][\"bar\"][3]", root[1]["bar"][3].u64());
+    printf("%-20s%lx\n", "root[3][1][4][1]", root[3][1][4][1].u64());
+    // also persistent in deeper paths!
+    printf("%-20s%lx\n", "root[0][3]", root[0][3].u64());
 
     // reshuffling persistently changes node's rng
-    printf("%-20s%llx\n", "root[42]", root[42].u64());
+    printf("%-20s%lx\n", "root[42]", root[42].u64());
     root[42].reshuffle();
     printf("%-20sreshuffle\n", "root[42]");
-    printf("%-20s%llx\n", "root[42]", root[42].u64());
+    printf("%-20s%lx\n", "root[42]", root[42].u64());
     return 0;
 }
